430fb472f9393247fb192bdc089c251a54751cf8
cosmearcade8-hue <cosmearcade8@gmail.com>
2025-12-10 14:49:01 +0800

    this may first task

---
+++ my-project/src/App.tsx
@@ -0,0 +1,53 @@
+import './App.css'
+
+import '@mantine/core/styles.css'
+
+import { TextInput } from '@mantine/core';
+import { useState } from 'react';
+import { NativeSelect } from '@mantine/core';
+import { Button } from '@mantine/core';
+import {IconDownload,  } from '@tabler/icons-react';
+import { IconSearch } from '@tabler/icons-react';
+
+
+import { SimpleGrid } from '@mantine/core';
+function App() {
+  const [value, setValue] = useState('');
+
+  return (
+    <>
+
+
+
+
+      
+  <SimpleGrid cols={3} spacing="xs" verticalSpacing="xs">
+      <div>   <TextInput
+
+      placeholder="Search"
+      leftSection={<IconSearch size={20} />} 
+    
+    /> </div>
+      <div> <NativeSelect
+        value={value}
+        onChange={(event) => setValue(event.currentTarget.value)}
+        data={['New Student', 'Transferee']}
+      /> </div>
+      <div><Button 
+        rightSection={<IconDownload size={20} />} 
+       variant="filled" color="rgba(255, 170, 0, 1)"
+        
+      >
+        export
+      </Button></div>
+    
+    </SimpleGrid>
+      
+    </>
+  );
+}
+
+export default App;
+
+
+
